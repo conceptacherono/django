5cb6a530dc0d628cdaf5e7316da699921e1f0c83
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-03-27 23:15:19 +0300

    defined images and search function in views.py in journey

--- voyage/journey/views.py
+++ voyage/journey/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
+def images(request):
+
+    images=Image.objects.all()
+   
+
+    return render(request,'images.html',{"images":images})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request,'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})    

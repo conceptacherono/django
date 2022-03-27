96f042284fb476edeaff9a1224389f91a90e9eb3
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-03-27 23:37:10 +0300

    added class in models.py

--- voyage/journey/models.py
+++ voyage/journey/models.py
@@ -3,3 +3,29 @@ import datetime as dt
 from django.db.models import Q
 
 # Create your models here.
+class Image(models.Model):
+    title=models.CharField(max_length=60)
+    categories = models.ManyToManyField(Category)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    #image = models.ImageField(upload_to='images/')
+    image = CloudinaryField('image')
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        # images = cls.objects.filter(categories__icontains=search_term)
+        images = cls.objects.filter(Q(categories__category=search_term) | Q(title__icontains=search_term) | Q(location__location=search_term))
+
+        return images    
+
+    def __str__(self):
+        return self.title        

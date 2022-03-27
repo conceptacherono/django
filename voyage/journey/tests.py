b11908c3cc8f7bca92ff1544a285cfcc772f9f5e
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-03-28 02:26:40 +0300

    created test for location

--- voyage/journey/tests.py
+++ voyage/journey/tests.py
@@ -3,3 +3,22 @@ from .models import Location,Category,Image
 import datetime as dt
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Moringa = Location(location='London')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Moringa,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_method(self):
+        self.Moringa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        self.Moringa.delete_location('London')
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
